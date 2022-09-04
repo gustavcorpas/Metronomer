61ce429313eb7ae890d6bb6f3fcd8d64a204eda0
Anton <dev@atjn.dk>
2022-09-04 21:36:22 +0200

    Also make new code better

--- functionality.js
+++ functionality.js
@@ -1,6 +1,6 @@
 let metronome;
 
-let storedSettings = JSON.parse(localStorage.getItem('settings')) || {};
+const storedSettings = JSON.parse(localStorage.getItem('settings')) || {};
 
 let bpm = parseInt(storedSettings.bpm) || 80;
 let freq = parseInt(storedSettings.frequency) || 220;
@@ -75,52 +75,52 @@ inputFreq.addEventListener('input', e => {
   }
 }, {passive: true});
 
-  // SETUP DYNAMIC BPM CHANGER
-  const rangeSliderThumbAdjust = document.querySelector('#range-slider-thumb-adjust');
-  const inputAdjust = document.querySelector('#input-adjust');
-  const pAdjust = document.querySelector('#p-adjust');
+// SETUP DYNAMIC BPM CHANGER
+const rangeSliderThumbAdjust = document.querySelector('#range-slider-thumb-adjust');
+const inputAdjust = document.querySelector('#input-adjust');
+const pAdjust = document.querySelector('#p-adjust');
 
-  rangeSliderThumbAdjust.style.width = `${(freq - MIN_FREQ) / (MAX_FREQ - MIN_FREQ) * 100}%`;
-  inputAdjust.value = adjust;
+rangeSliderThumbAdjust.style.width = `${(freq - MIN_FREQ) / (MAX_FREQ - MIN_FREQ) * 100}%`;
+inputAdjust.value = adjust;
+pAdjust.textContent = adjust;
+
+inputAdjust.addEventListener('input', e => {
+  adjust = e.target.value;
   pAdjust.textContent = adjust;
+  rangeSliderThumbAdjust.style.width = `${(adjust) / MAX_ADJUST * 100}%`;
+  registerAdjustChange(adjust, false);
+}, {passive: true});
 
-  inputAdjust.addEventListener('input', e => {
-    adjust = e.target.value;
-    pAdjust.textContent = adjust;
-    rangeSliderThumbAdjust.style.width = `${(adjust) / MAX_ADJUST * 100}%`;
-    registerAdjustChange(adjust, false);
-  });
+let adjustWaiting = false;
 
-  let adjustWaiting = false;
+function registerAdjustChange(val, cb){
+  if(!cb && adjustWaiting === true) return;
+  adjustWaiting = true;
+  if(cb && val === adjust) { adjustWaiting = false; return; }
+  setTimeout(registerAdjustChange, 1000, adjust, true);
 
-  function registerAdjustChange(val, cb){
-    if(!cb && adjustWaiting == true) return;
-    adjustWaiting = true;
-    if(cb && val == adjust) { adjustWaiting = false; return; }
-    setTimeout(registerAdjustChange, 1000, adjust, true);
-
-    // ADJUST CHANGED
-    console.log("adjust changed to: " + adjust);
-    clearInterval(adjustInterval);
-    let s = (adjust / 60);
-    if(s != 0){
-      adjustInterval = setInterval(adjustBPM, 1 / s * 1000 );
-    }
-
+  // ADJUST CHANGED
+  console.log("adjust changed to: " + adjust);
+  clearInterval(adjustInterval);
+  const s = (adjust / 60);
+  if(s !== 0){
+    adjustInterval = setInterval(adjustBPM, 1 / s * 1000 );
   }
 
-  function adjustBPM(){
-    console.log("adjust");
-    if( metronome && bpm != MAX_BPM && playing ){
-      bpm += 1;
-      metronome.settings({bpm: bpm});
-      pBpm.textContent = bpm;
-      rangeSliderThumbBpm.style.width = `${bpm / MAX_BPM * 100}%`;
-      inputBpm.value = bpm;
-    }
-    console.log("done");
+}
 
+function adjustBPM(){
+  console.log("adjust");
+  if( metronome && bpm !== MAX_BPM && playing ){
+    bpm += 1;
+    metronome.settings({bpm: bpm});
+    pBpm.textContent = bpm;
+    rangeSliderThumbBpm.style.width = `${bpm / MAX_BPM * 100}%`;
+    inputBpm.value = bpm;
   }
+  console.log("done");
+
+}
 
 
 
