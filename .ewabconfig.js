2afeb21305bec76ba355058f4fb5292ce0a1774f
Gustav Corpas <gustavcorpas@gmail.com>
2022-09-25 18:18:02 +0200

    add build step and ewap

---
+++ .ewabconfig.js
@@ -0,0 +1,17 @@
+
+/**
+ * @file
+ * Configuration script for easy-web-app-builder.
+ */
+
+export default {
+  "fileExceptions": [],
+  "icons": {
+    "add": true
+  },
+  "files": {
+    "minify": true,
+    "addSourceMaps": false
+  },
+  "serviceworker": {"add": true},
+}
